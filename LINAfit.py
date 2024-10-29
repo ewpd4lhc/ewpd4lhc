ff87d5a28d16a81e284c6ea3827e3e25260144d8
Hannes Mildner <hannes.mildner.cern@gmail.com>
2024-10-29 13:53:58 +0100

    make LINAfit return normal python floats

--- LINAfit.py
+++ LINAfit.py
@@ -105,7 +105,6 @@ def lina_fit(names, measured, predicted, covariance, parameters, parametrization
                 maxproj = max(maxproj, abs(np.dot(p, d) / pnorm / dnorm))
             if maxproj > unconstrainedcutoff:
                 unconstrained.append(n)
-
     # Reparametrize if sensitive directions have been identified
     # In that case eigenvectors are the parameters
     if repara is not None:
@@ -118,8 +117,8 @@ def lina_fit(names, measured, predicted, covariance, parameters, parametrization
     covP = np.linalg.inv(paraT @ covI @ para)
 
     # Convert results to dictionaries
-    dparam_res = {n: r for n, r in zip(params, param_res)}
-    dpara_cov = {n: {m: covP[i][j] for j, m in enumerate(
+    dparam_res = {n: float(r) for n, r in zip(params, param_res)}
+    dpara_cov = {n: {m: float(covP[i][j]) for j, m in enumerate(
         params)} for i, n in enumerate(params)}
 
     # Post-fit residuals for observables
@@ -128,9 +127,9 @@ def lina_fit(names, measured, predicted, covariance, parameters, parametrization
     cov_postfit = para @ covP @ paraT
 
     # Post fit result (subtract residual)
-    dres = {n: m - r for n, r, m in zip(names, res, meas)}
+    dres = {n: float(m - r) for n, r, m in zip(names, res, meas)}
     # Post fit cov (create a dict from the matrix)
-    dcov = {n: {m: cov_postfit[i][j] for j, m in enumerate(
+    dcov = {n: {m: float(cov_postfit[i][j]) for j, m in enumerate(
         names)} for i, n in enumerate(names)}
 
     # If a reparametrization has been performed, information on sensitive directions is reported
